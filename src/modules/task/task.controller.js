9dac15be4cbabef6c4743c142a9d64254f6601e3
Fabricio Souza <fabricio.abner@gmail.com>
2026-01-02 13:21:25 -0300

    task: criando o controller

---
+++ src/modules/task/task.controller.js
@@ -0,0 +1,42 @@
+
+
+export async function index(req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function show(req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function store(req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function update(req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function destroy(req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
