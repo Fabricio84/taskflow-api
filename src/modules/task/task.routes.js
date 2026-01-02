845940cedc5af5cba39b33e1787adc6d49ebd634
Fabricio Souza <fabricio.abner@gmail.com>
2026-01-02 13:24:03 -0300

    task: implementando as rotas

--- src/modules/task/task.routes.js
+++ src/modules/task/task.routes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import authMiddleware from '../../middlewares/auth.middleware.js'
+import { index, show, store, update, destroy } from './task.controller.js'
 
 const router = express.Router()
 
-router.post('/tasks', authMiddleware, (req, res) => {})
-router.get('/tasks', authMiddleware, (req, res) => {})
-router.get('/tasks/:id', authMiddleware, (req, res) => {})
-router.put('/tasks/:id', authMiddleware, (req, res) => {})
-router.delete('/tasks/:id', authMiddleware, (req, res) => {})
+router.post('/tasks', authMiddleware, store)
+router.get('/tasks', authMiddleware, index)
+router.get('/tasks/:id', authMiddleware, show)
+router.put('/tasks/:id', authMiddleware, update)
+router.delete('/tasks/:id', authMiddleware, destroy)
 
 export default router
