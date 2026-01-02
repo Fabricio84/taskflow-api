83eb3851ae38ee47a826b854b7ca4b0483fbedcd
Fabricio Souza <fabricio.abner@gmail.com>
2026-01-02 13:43:13 -0300

    task: implementando o controller

--- src/modules/task/task.controller.js
+++ src/modules/task/task.controller.js
@@ -1,8 +1,16 @@
-
+import { getTasks, getTaskById, createTask, updateTaskById, deleteTask } from './task.service.js';
+import { BadRequestError } from '../../errors/BadRequestError.js';                                                                                                                                                                                                                                                                                                      
 
 export async function index(req, res, next) {
     try {
+        const userId = req.query.userId;
         
+        if (!userId)
+            throw new BadRequestError('User ID é orbigatório!');
+        
+        const tasks = await getTasks(userId);
+
+        res.status(200).json(tasks);
     } catch (error) {
         next(error)
     }
@@ -10,7 +18,17 @@ export async function index(req, res, next) {
 
 export async function show(req, res, next) {
     try {
-        
+        const { userId, taskId } = req.query
+ 
+        if (!userId)
+            throw new BadRequestError('User ID é orbigatório!');
+
+        if (!taskId)
+            throw new BadRequestError('Task ID é orbigatório!');
+
+        const task = await getTaskById(userId, taskId);
+
+        res.status(200).json(task);
     } catch (error) {
         next(error)
     }
@@ -18,7 +36,23 @@ export async function show(req, res, next) {
 
 export async function store(req, res, next) {
     try {
+        const { userId, titulo, descricao, prioridade } = req.body;
         
+        if (!userId)
+            throw new BadRequestError('User ID é orbigatório!');
+
+        if (!titulo)
+            throw new BadRequestError('Título é orbigatório!');
+
+        if (!descricao)     
+            throw new BadRequestError('Descrição é orbigatório!');
+
+        if (!prioridade)     
+            throw new BadRequestError('Prioridade é orbigatório!');
+
+        const newTask = await createTask(userId, titulo, descricao, prioridade);
+
+        res.status(201).json(newTask);    
     } catch (error) {
         next(error)
     }
@@ -26,7 +60,17 @@ export async function store(req, res, next) {
 
 export async function update(req, res, next) {
     try {
+        const { userId, taskId, titulo, descricao, prioridade, status} = req.body;
         
+        if (!userId)
+            throw new BadRequestError('User ID é orbigatório!');
+
+        if (!taskId)
+            throw new BadRequestError('Task ID é orbigatório!');
+
+        const updatedTask = await updateTaskById(userId, taskId, titulo, descricao, prioridade, status);
+
+        res.status(200).json(updatedTask);
     } catch (error) {
         next(error)
     }
@@ -34,7 +78,17 @@ export async function update(req, res, next) {
 
 export async function destroy(req, res, next) {
     try {
+        const { userId, taskId } = req.body;
         
+        if (!userId)
+            throw new BadRequestError('User ID é orbigatório!');
+
+        if (!taskId)
+            throw new BadRequestError('Task ID é orbigatório!');
+        
+        await deleteTask(userId, taskId);
+        
+        res.status(204).send();
     } catch (error) {
         next(error)
     }
--- src/modules/task/task.service.js
+++ src/modules/task/task.service.js
@@ -1,9 +1,9 @@
 import { prisma } from "../../config/prisma.js";
 
-const createTask = async (userId, title, description) => {};
+const createTask = async (userId, titulo, decricao, prioridade) => {};
 const getTasks = async (userId) => {};
 const getTaskById = async (userId, taskId) => {};
-const updateTask = async (userId, taskId, title, description) => {};
+const updateTask = async (userId, taskId, titulo, descricao, prioridade, status) => {};
 const deleteTask = async (userId, taskId) => {};
 
 export default { createTask, getTasks, getTaskById, updateTask, deleteTask }
